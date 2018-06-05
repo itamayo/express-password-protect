def8435f143e0fb29affea06c4c225b73288e72f
Jonathan D. Mann <jdmann84@gmail.com>
2018-06-05 13:44:18 -0700

    added maxAge parameter and improved CSS styling

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ const express = require('express')
 const app = express()
 const session = require('express-session')
 const bodyParser = require('body-parser')
+const pug = require('pug')
+
 // define middleware functions
 let PP = {
     session: 
-    	session({
-			secret: 'passwordProtected',
-			resave: false,
-			saveUninitialized: true,
-			unset: 'destroy',
-			name: 'passwordProtected',
-			cookie: {
-				maxAge: 1000 * 60 * 60 * 24 // 1 day
-			}
-		}),
+        function(options) {
+            return session({
+                secret: 'passwordProtected',
+                resave: false,
+                saveUninitialized: true,
+                unset: 'destroy',
+                name: 'passwordProtected',
+                cookie: {
+                    maxAge: (options.maxAge) ? options.maxAge : (1000 * 60 * 60 * 24) // default is 1 day
+                }
+            })
+        },   
     configure: 
     	function(options) {
     		return function(req, res, next) {
@@ -30,22 +34,18 @@ let PP = {
     	},    
     login: 
     	function(req, res, next) {
-    		console.log("body   ", req.body)
 			// check if logged in using session, continue to other express routes in app    		
     		if(req.session.PP && req.session.PP.loggedIn) {
-				console.log('logged in already')
     			next()
     		}
 			// attempting to login using password protected login form; check username and password; if so, let proceed and save loggedIn as true   		
 			else if(req.body && (req.body.PPU1 == req.session.PP.username) && (req.body.PPP1 == req.session.PP.password)) {
 				req.session.PP.loggedIn = true
-				console.log('logged in just now')
 				next()
 			}
 			// deny access
 			else {
-				console.log('denied access')
-				res.send('<html><body><form method="post" action="/login"><input type="text" name="PPU1"/><input type="text" name="PPP1" value="test"/><button type="submit"></form>')
+				res.send(pug.renderFile(__dirname + '/loginPage.pug'))
 			}		
 
     	}
@@ -53,5 +53,5 @@ let PP = {
 
 // return multiple middleware functions as array inside function with options parameter
 module.exports = function(options) {
-  return [bodyParser.urlencoded({extended: true}), PP.session, PP.configure(options), PP.login]
+  return [bodyParser.urlencoded({extended: true}), PP.session(options), PP.configure(options), PP.login]
 }
